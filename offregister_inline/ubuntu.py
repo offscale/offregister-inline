8c57b39f001730f4f98733649a274b1644539b59
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_inline/ubuntu.py
+++ offregister_inline/ubuntu.py
@@ -1,12 +1,14 @@
-from itertools import imap
-
 from fabric.context_managers import shell_env, cd
 from fabric.operations import sudo, run
 
 
 def step0(*args, **kwargs):
-    with cd(kwargs.get('CWD', '$HOME')), shell_env(**kwargs.get('ENV', {})):
-        return {k: tuple(v)
-                for k, v in (('run', imap(run, kwargs['run']) if 'run' in kwargs else None),
-                             ('sudo', imap(sudo, kwargs['sudo']) if 'sudo' in kwargs else None))
-                if v is not None}
+    with cd(kwargs.get("CWD", "$HOME")), shell_env(**kwargs.get("ENV", {})):
+        return {
+            k: tuple(v)
+            for k, v in (
+                ("run", list(map(run, kwargs["run"])) if "run" in kwargs else None),
+                ("sudo", list(map(sudo, kwargs["sudo"])) if "sudo" in kwargs else None),
+            )
+            if v is not None
+        }
