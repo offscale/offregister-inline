012980a5f87eb4eb8d73290d9e9db25f968b951d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-03-31 17:51:55 -0400

    [*.py] Explicitly set encoding to fix Python 2.7 issues / potential-issues

--- offregister_inline/__init__.py
+++ offregister_inline/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = "Samuel Marks"
 __version__ = "0.0.4"
 __description__ = "inline module for Fabric (offregister)"
--- offregister_inline/debian.py
+++ offregister_inline/debian.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from offregister_inline.deb_shared import *
 
 __all__ = ["step0"]
